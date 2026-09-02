7527826dbe547196899ec99334b5660af33ba995
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2026-09-02 14:09:23 -0300

    Atualiza o export web do Godot (zigue da estrela, sifonóforo do leviatã, clarão do dano e chefes a 12 fps)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788354363|2589066';
+const CACHE_VERSION = '1788368772|4143585';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
