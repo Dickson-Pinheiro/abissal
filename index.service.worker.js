1178665af250ea244eaf2518e137f4fdd6125dd6
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 07:38:50 -0300

    Atualiza o export web do Godot (Baiacu: sprite animada)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788431576|9045658';
+const CACHE_VERSION = '1788431909|9046865';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
