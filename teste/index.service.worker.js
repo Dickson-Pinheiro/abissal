3732fb0538c97f8cc47c3b2abbe8dd47226621ed
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 16:11:09 -0300

    Atualiza /teste/: Serpente mais difícil (frequência, quantidade, entra-e-sai)

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788462065|9209071';
+const CACHE_VERSION = '1788462651|9360557';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
