6636565aa398cd7c9efaa8c96a5233598876da65
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 19:55:25 -0300

    teste: descida oculta com chances e chefes finais

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788475224|9737561';
+const CACHE_VERSION = '1788476117|9682526';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
