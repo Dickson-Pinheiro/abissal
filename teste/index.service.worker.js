449b605896527a6cf1b54c656cce28365b039f3d
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 16:24:45 -0300

    Atualiza /teste/: corpo inteiro da Serpente machuca por contato

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788462651|9360557';
+const CACHE_VERSION = '1788463465|9190807';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
