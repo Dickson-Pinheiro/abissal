695876c8d3a9133b0d838b9cfd107e959e7bd38e
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 13:11:12 -0300

    Atualiza /teste/: A Serpente Cega, chefe novo

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788446774|9273981';
+const CACHE_VERSION = '1788451851|9358259';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
