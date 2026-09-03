fa6dc8c3507fcb34abafbf62b6b015a0508fecb4
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 07:33:19 -0300

    Atualiza o export web do Godot (Baiacu: mais velocidade)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788431022|9055246';
+const CACHE_VERSION = '1788431576|9045658';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
