f01d21a62695ff9616862437114f3f01f15e53ce
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 18:56:59 -0300

    teste: descida oculta (chefe secundario opcional)

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788463465|9190807';
+const CACHE_VERSION = '1788472586|9791901';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
