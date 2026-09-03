d6bae023e897d52483a89553149fa2291a787b99
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 08:56:54 -0300

    Atualiza o export web do Godot (A Fenda: trilha provisória)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788436374|9151133';
+const CACHE_VERSION = '1788436591|9134452';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
