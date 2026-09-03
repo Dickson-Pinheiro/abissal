0c87d459c56be640dd719870accdd6437a843152
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-02 21:23:03 -0300

    Atualiza o export web do Godot (Anêmona, cardume redesenhado, inimigos presos na tela)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788383995|8555140';
+const CACHE_VERSION = '1788394945|12100944';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
