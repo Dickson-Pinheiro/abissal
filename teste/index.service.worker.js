07d84b8a96f54917df83b9aa3da3388a52429667
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 13:53:35 -0300

    Atualiza /teste/: A Serpente Cega com sprite animada

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788452091|9168430';
+const CACHE_VERSION = '1788454395|9125915';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
