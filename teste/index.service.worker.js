7efab9eab3b9ee3303e99055a598eb09fa4068ef
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 14:06:45 -0300

    Atualiza /teste/: Serpente com corpo em segmentos

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788454395|9125915';
+const CACHE_VERSION = '1788455186|9255953';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
