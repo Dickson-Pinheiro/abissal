9f1eceebc4a4ed999da227fcddb0494133ae77da
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 21:41:41 -0300

    teste: corrige dano por contato dos chefes (toque_t nunca decaia)

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788476117|9682526';
+const CACHE_VERSION = '1788482491|9705849';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
