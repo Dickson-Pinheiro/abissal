731eaa6b8b5252a591958a177956d9c7280d30d3
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-03 22:13:04 -0300

    teste: habilidades unicas por chefe opcional

--- teste/index.service.worker.js
+++ teste/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788482491|9705849';
+const CACHE_VERSION = '1788484375|9648791';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
