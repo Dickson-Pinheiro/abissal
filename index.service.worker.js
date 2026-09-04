3ce5d93f2bdd0e73de7134b935745bbf236a409b
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-04 20:00:49 -0300

    Atualiza o export web do Godot (Cerco Abissal e Emboscada Bífida: projéteis verde-lima, cor da Serpente)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788562142|15348425';
+const CACHE_VERSION = '1788562821|8566365';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
