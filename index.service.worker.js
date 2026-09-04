83e9b68862dc7602f2a219ccd9777023207e7ea8
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-04 19:49:40 -0300

    Atualiza o export web do Godot (investida da serpente: anel verde-lima ao redor do casco durante o dash)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788558651|9699225';
+const CACHE_VERSION = '1788562142|15348425';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
