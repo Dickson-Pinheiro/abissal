bf4fcf3dec0fabca704584b449efcec17db756ac
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-04 18:41:16 -0300

    Atualiza o export web do Godot (investida da serpente: efeito verde-lima ao acertar)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788487255|10028636';
+const CACHE_VERSION = '1788558007|9555064';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
