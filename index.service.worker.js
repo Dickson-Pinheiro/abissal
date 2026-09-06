ba9f17219de6a6318111ac28ea43a106d91816c3
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-06 11:52:09 -0300

    Atualiza o export web do Godot (clarão de dano mais sutil, aplicado a todas as criaturas)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788562821|8566365';
+const CACHE_VERSION = '1788706259|10040520';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
