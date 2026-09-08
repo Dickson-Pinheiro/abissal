d201c146b9208dfe2857149f4127168d170b3443
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-08 12:22:58 -0300

    Atualiza o export web do Godot (corrige chefe oculto duplicado no clique duplo)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788873976|9857496';
+const CACHE_VERSION = '1788880952|9969811';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
