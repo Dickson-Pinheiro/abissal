a32065354b478e07b10bcadd0437581c9a890fd2
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-08 13:43:03 -0300

    Atualiza o export web do Godot (habilidades da Trindade, oculta na 3a camada)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1788880952|9969811';
+const CACHE_VERSION = '1788885758|10089127';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
