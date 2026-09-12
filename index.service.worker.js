54d5e941c64d0bdaf430c793c094c824e931e9da
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-11 22:38:07 -0300

    Atualiza o export web do Godot (5 chefes ocultos migrados para asset) (2026-09-11 22:36)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789163597|11706474';
+const CACHE_VERSION = '1789177055|12297546';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
