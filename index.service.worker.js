4cb80e2b48ac5c920ff339f65873f9765b589984
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-13 14:45:01 -0300

    Atualiza o export web do Godot (novo visual da Fome sem Fundo, peixe-pescador em vez de decalque da Goela)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789320968|13483258';
+const CACHE_VERSION = '1789321474|13479279';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
