9d5164f9371b8a023a1f48765c3bdf12e05e7a2e
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-13 15:32:29 -0300

    Atualiza o export web do Godot (O Cardume Voraz, terceira opção oculta da Zona Crepuscular)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789322686|13605886';
+const CACHE_VERSION = '1789324324|13777916';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
