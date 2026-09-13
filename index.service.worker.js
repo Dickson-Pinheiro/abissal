439f9b168783c0f3ff081bfd05dddf3973a25b29
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-13 15:42:01 -0300

    Atualiza o export web do Godot (O Véu Lacerado, quarta opção oculta da Zona Crepuscular)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789324324|13777916';
+const CACHE_VERSION = '1789324895|14200093';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
