52e14a05f7e98534e4073754ed8bd76a1599239d
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-13 15:51:21 -0300

    Atualiza o export web do Godot (A Concha Blindada — a Zona Crepuscular fecha as 5 candidatas ocultas)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789324895|14200093';
+const CACHE_VERSION = '1789325452|14666697';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
