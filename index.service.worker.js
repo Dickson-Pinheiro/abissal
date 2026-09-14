23b16e53acfae0803febc1b1979829ea730f7816
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-14 14:56:27 -0300

    Atualiza o export web do Godot (descida oculta: dois pontos obrigatórios por camada, sem repetir candidata, hordas aumentadas)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789407216|18442382';
+const CACHE_VERSION = '1789408556|17719609';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
