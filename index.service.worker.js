0a9dd322e5681aabc26d03a282f2bfd342e26936
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-17 11:00:53 -0300

    Atualiza o export web do Godot (aumenta 6 hordas em cada camada)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789648521|18204008';
+const CACHE_VERSION = '1789653634|18172868';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
