cd7201f1ebe897c6b01d97b0728a677ca9103509
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-17 09:35:41 -0300

    Atualiza o export web do Godot (inverte a direção das faixas do fundo do menu: sobem, não descem)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789641910|18009792';
+const CACHE_VERSION = '1789648521|18204008';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
