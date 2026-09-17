9335a912523c550502ed2a821d55128ced425f21
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-17 07:45:28 -0300

    Atualiza o export web do Godot (reposiciona ABISSAL entre o fim dos botões e a borda direita)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789641584|18000275';
+const CACHE_VERSION = '1789641910|18009792';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
