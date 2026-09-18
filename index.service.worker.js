3e7c79c7e6407d70dac5d48e4e876e7893aef3c7
Dennis Pinheiro <dennispinheiro97@gmail.com>
2026-09-18 12:35:36 -0300

    Reverte o export web para antes da transição pós-chefe (travava ao apertar Descer)

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1789665843|4315807';
+const CACHE_VERSION = '1789745597|20940888';
 /** @type {string} */
 const CACHE_PREFIX = 'Abissal-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
